8823d6f152759b458bd4102bd382c3bd4819506b
kuehne-trustable-de <e8e72298ab5a8898c9c85107023174dd0e2806d2>
2023-07-27 14:09:16 +0200

    renovabot updates supression for jackson-databind-2.15.2, keeping it until spring boot v3 version alignment

---
+++ adcsCertUtil/src/main/java/de/trustable/ca3s/adcsCertUtil/ADCSRetryException.java
@@ -0,0 +1,15 @@
+package de.trustable.ca3s.adcsCertUtil;
+
+
+public class ADCSRetryException extends Exception {
+
+	public ADCSRetryException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3436745891405664383L;
+
+}
